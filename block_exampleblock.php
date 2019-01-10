6e17dd225009f1a2cf30e01efb36f5b99744779e
Sabine Maennel <sabine.maennel@gmail.com>
2019-01-10 21:13:49 +0100

    added modifieable content to the block

--- block_exampleblock.php
+++ block_exampleblock.php
@@ -5,4 +5,22 @@ class block_exampleblock extends block_base {
     }
     // The PHP tag and the curly bracket for the class definition
     // will only be closed after there is another function added in the next section.
+    public function get_content() {
+        if ($this->content !== null) {
+            return $this->content;
+        }
+
+        $this->content         =  new stdClass;
+
+        // get configured text if available
+        if (! empty($this->config->text)) {
+            $this->content->text = $this->config->text;
+        }
+
+        $this->content->text   = 'The content of our SimpleHTML block!';
+        $this->content->footer = 'Footer here...';
+
+        return $this->content;
+
+    }
 }
--- lang/en/block_exampleblock.php
+++ lang/en/block_exampleblock.php
@@ -3,3 +3,4 @@ $string['pluginname'] = 'Example block';
 $string['exampleblock'] = 'Example for a block';
 $string['exampleblock:addinstance'] = 'Add a new example block';
 $string['exampleblock:myaddinstance'] = 'Add a new example to the My Moodle page';
+$string['blockstring'] = 'Please add a string';
